4edeacd7888d236d470092f1d5b4097d1c01749e
ferni232 <fernandorudnevich.inedita@gmail.com>
2022-08-31 23:38:04 -0300

    cambios

---
+++ src/index.js
@@ -0,0 +1,24 @@
+const express = require("express")
+const productosRoutes = require("./routes/routesProductos");
+const cartsRoutes = require("./routes/routesCarrito");
+
+const app = express()
+const port = process.env.PORT || 8080
+
+//midelwars de aplicacion
+app.use(express.json())
+app.use(express.urlencoded({extended:false}))//para decodificar la url .
+
+
+
+app.set("view engine" ,"ejs")
+app.set("views","./views")
+
+app.use("/",productosRoutes);
+app.use("/api/productos",productosRoutes);
+app.use("/api/cart",cartsRoutes);
+
+
+app.listen(port,()=>{
+    console.log("server ok")
+})
